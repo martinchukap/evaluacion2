86699919ca58c75d6a95b9b8e969e265665bf943
Laboratorios Chillan <chilab@inacap.cl>
2023-10-19 09:39:44 -0300

    creacion de paises.html

--- carech/urls.py
+++ carech/urls.py
@@ -3,10 +3,17 @@ from django.contrib import admin
 from django.urls import path
 
 from primerapp.views import *
+from segundapp.views import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
     path("juguetes/", juguetes),
     path("electronica/", electronica),
     path("ropa/", ropa),
+
+
+    path("chile/", chile),
+    path("argentina/", argentina),
+    path("brasil/", brasil),
 ]
